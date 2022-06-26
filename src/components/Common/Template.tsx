b11cf24904baa71721a08b2bf2040b0ecdcd8bd5
kim <dog1239121@gmail.com>
2022-06-26 10:09:53 +0900

    Google Search Console에 등록

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -45,8 +45,13 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={image} />
-        <meta name="twitter:site" content="@사용자이름" />
-        <meta name="twitter:creator" content="@사용자이름" />
+        <meta name="twitter:site" content="@username" />
+        <meta name="twitter:creator" content="@username" />
+
+        <meta
+          name="google-site-verification"
+          content="웹 마스터 도구가 제공하는 Meta 태그"
+        />
 
         <html lang="ko" />
       </Helmet>
